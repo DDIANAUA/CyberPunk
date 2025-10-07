be7762d4594cb14482170b62b6715a205fd87012
DDIANAUA <dianatsapenko06@gmail.com>
2025-10-07 18:13:40 +0300

    new commit

--- scripts/main.js
+++ scripts/main.js
@@ -15,6 +15,6 @@ class BackgroundRotator {
 	}
 }
 
-const backgroundImages = ['./../images/hero/0.avif', './../images/hero/1.avif', './../images/hero/2.avif'];
+const backgroundImages = ['./images/hero/0.avif', './images/hero/1.avif', './images/hero/2.avif'];
 
 new BackgroundRotator('#hero-section', backgroundImages, 3000);
